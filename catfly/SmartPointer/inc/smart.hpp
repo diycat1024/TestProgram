50f419f79ba390b59fef66e5029d7d7fb323a4c5
zqj <582132116@qq.com>
2020-06-05 15:01:12 +0800

    refactor: 智能指针
    智能指针类加上模板

--- catfly/SmartPointer/inc/smart.hpp
+++ catfly/SmartPointer/inc/smart.hpp
@@ -2,39 +2,33 @@
 
 using namespace std;
 
-class Point                                       
-{
-public:
-	Point(int xVal = 0, int yVal = 0) :x(xVal), y(yVal) { }
-	int getX() const { return x; }
-	int getY() const { return y; }
-	void setX(int xVal) { x = xVal; }
-	void setY(int yVal) { y = yVal; }
+template <typename T>
+class SmartPtr;
 
-private:
-	int x, y;
-};
 
+template <typename T>
 class U_Ptr
 {
     private:
-    friend class SmartPtr;
-    U_Ptr(Point *ptr):p(ptr),count_(1){}
+    friend class SmartPtr<T>;
+    U_Ptr(T *ptr):p(ptr),count_(1){}
     ~U_Ptr(){delete p;}
 
     int count_;
-    Point *p;
+    T *p;
 };
 
 
+
+template <typename T>
 class SmartPtr
 {
 public:
-	SmartPtr(Point *ptr) :rp(new U_Ptr(ptr)) { }    
+	SmartPtr(T *ptr) :rp(new U_Ptr<T>(ptr)) { }    
 	
-	SmartPtr(const SmartPtr &sp) :rp(sp.rp) { ++rp->count_; }
+	SmartPtr(const SmartPtr<T> &sp) :rp(sp.rp) { ++rp->count_; }
 	  
-	SmartPtr& operator=(const SmartPtr& rhs) {    
+	SmartPtr& operator=(const SmartPtr<T>& rhs) {    
 		++rp->count_;    
 		if (--rp->count_ == 0)    
 			delete rp;
@@ -49,16 +43,16 @@ public:
 		cout << "还有" << rp->count_ << "个指针指向基础对象" << endl;
 	}
 
-    Point& operator * () //重载*操作符  
+    T& operator * () //重载*操作符  
     {
         return *(rp->p);
     }
-    Point* operator -> () //重载->操作符  
+    T* operator -> () //重载->操作符  
     {
         return rp->p;
     }
 
 	
 private:
-        U_Ptr *rp;  
+        U_Ptr<T> *rp;  
 };
--- catfly/SmartPointer/src/main.cpp
+++ catfly/SmartPointer/src/main.cpp
@@ -3,18 +3,20 @@
 int main()
 {
     //定义一个基础对象类指针
-	Point *pa = new Point(10, 20);
+	int *i = new int(1);
 
 	//定义三个智能指针类对象，对象都指向基础类对象pa
 	//使用花括号控制三个指针指针的生命期，观察计数的变化
 
 	{
-		SmartPtr sptr1(pa);//此时计数count=1
-	    cout << pa->getX ()<< endl;
+		SmartPtr<int> sptr1(i);//此时计数count=1
 		{
-			SmartPtr sptr2(sptr1); //调用复制构造函数，此时计数为count=2
+			SmartPtr<int> sptr2(sptr1); //调用复制构造函数，此时计数为count=2
 			{
-				SmartPtr sptr3=sptr1; //调用赋值操作符，此时计数为conut=3
+				SmartPtr<int> sptr3=sptr1; //调用赋值操作符，此时计数为conut=3
+                cout << *sptr1 << endl;
+				*sptr1 = 20;
+				cout << *sptr2 << endl;
 			}
 			//此时count=2
 		}
@@ -22,7 +24,6 @@ int main()
 	}
 	//此时count=0；pa对象被delete掉
 
-	cout << pa->getX ()<< endl;
 
 	system("pause");
 	return 0;
